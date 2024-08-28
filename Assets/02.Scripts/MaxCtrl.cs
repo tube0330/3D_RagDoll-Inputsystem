016fe3ffad40f970dea3d707e201d789a69a2132
tube0330 <zien0516@naver.com>
2024-08-28 15:38:54 +0900

    InputSystem

--- Assets/02.Scripts/MaxCtrl.cs
+++ Assets/02.Scripts/MaxCtrl.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MaxCtrl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    new Transform transform;
+    Animator ani;
+    Vector3 moveDir;
+
     void Start()
     {
-        
+        transform = GetComponent<Transform>();    
+        ani = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(moveDir != Vector3.zero)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * 3.0f);
+            transform.rotation = Quaternion.LookRotation(moveDir);
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 dir = value.Get<Vector2>();
+        moveDir = new Vector3(dir.x, 0f, dir.y);
+        //ani.SetFloat("Speed", dir.magnitude);
     }
 }
